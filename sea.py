6be7c375cd8eb92bbfc59c57a8a45ccd7923d68e
Dillon Arevalo <dillonbarevalo@gmail.com>
2018-06-02 18:05:33 -0700

    Clean up spacing in sea.py

--- sea.py
+++ sea.py
@@ -1,9 +1,9 @@
 #! /usr/bin/env python
-# 
+#
 # sea - console frontend to the SeaIce metadictionary
 #
 # Copyright (c) 2013, Christopher Patton, all rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
 #   * Redistributions of source code must retain the above copyright
@@ -13,7 +13,7 @@
 #     documentation and/or other materials provided with the distribution.
 #   * The names of contributors may be used to endorse or promote products
 #     derived from this software without specific prior written permission.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
 # ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 # WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -56,9 +56,9 @@ parser.description = description
 parser.add_option("-d", "--dump", action="store_true", dest="dump", default=False,
                   help="Display dictionary contents on the terminal.")
 
-parser.add_option("--config", dest="config_file", metavar="FILE", 
-                  help="User credentials for local PostgreSQL database (defaults to '$HOME/.seaice'). " + 
-                       "If 'heroku' is given, then a connection to a foreign host specified by " + 
+parser.add_option("--config", dest="config_file", metavar="FILE",
+                  help="User credentials for local PostgreSQL database (defaults to '$HOME/.seaice'). " +
+                       "If 'heroku' is given, then a connection to a foreign host specified by " +
                        "DATABASE_URL is established.",
                   default='heroku')
 
@@ -80,7 +80,7 @@ parser.add_option("--export", dest="export_table",
                   help="Export dictionary as JSON-formatted FILE.",
                   metavar="TABLE")
 
-parser.add_option("-f", "--file", dest="file_name", 
+parser.add_option("-f", "--file", dest="file_name",
                   help="File from which to import, or file to which to export.",
                   metavar="FILE")
 
@@ -102,7 +102,7 @@ parser.add_option("--set-reputation", dest="reputation",
                   help="Set user's reputation (to a positive integer).",
                   metavar="INT")
 
-parser.add_option("-u", "--user", dest="user_id", 
+parser.add_option("-u", "--user", dest="user_id",
                   help="ID of user whose reputation is to be set.",
                   metavar="ID", default=None)
 
@@ -118,7 +118,7 @@ parser.add_option("-j", "--json", action="store_true", dest="json", default=Fals
 parser.add_option("-q", "--quiet", action="store_true", dest="quiet", default=False,
                   help="Don't prompt for confirmation when dropping tables.")
 
-parser.add_option("--role", dest="db_role", metavar="USER", 
+parser.add_option("--role", dest="db_role", metavar="USER",
                   help="Specify the database role to use for the DB connector pool. These roles " +
                        "are specified in the configuration file (see --config).",
                   default="default")
@@ -139,21 +139,21 @@ if len(sys.argv) < 2:
 
 try:
 
-  if options.config_file == "heroku": 
-    
+  if options.config_file == "heroku":
+
     sea = seaice.SeaIceConnector()
 
-  else: 
-    try: 
+  else:
+    try:
       config = seaice.auth.get_config(options.config_file)
-    except OSError: 
+    except OSError:
       print >>sys.stderr, "error: config file '%s' not found" % options.config_file
       sys.exit(1)
 
-    sea = seaice.SeaIceConnector(config.get(options.db_role, 'user'),       
+    sea = seaice.SeaIceConnector(config.get(options.db_role, 'user'),
                                  config.get(options.db_role, 'password'),
                                  config.get(options.db_role, 'dbname'))
-  
+
   ## Run specified queries. ##
 
   if options.remove_id:
@@ -162,7 +162,7 @@ try:
 
   if options.reputation:
     if not options.user_id:
-      print >>sys.stderr, "sea: must specify user Id" 
+      print >>sys.stderr, "sea: must specify user Id"
     elif not sea.updateUserReputation(int(options.user_id), int(options.reputation)):
       print >>sys.stderr, "sea: no such user (Id=%s not found)" % options.user_id
 
@@ -175,52 +175,52 @@ try:
 
   if options.export_table:
     sea.Export(options.export_table, options.file_name)
-  
+
   if options.drop_tables:
     if not options.quiet:
       print >>sys.stderr, "warning: this will erase all terms. Are you sure? [y/n] ",
-      if raw_input() in ['y', 'yes', 'Y']: 
+      if raw_input() in ['y', 'yes', 'Y']:
         sea.dropSchema()
     else:
       sea.dropSchema()
-        
+
   if options.create_tables:
     sea.createSchema()
-  
-  if options.dump: 
+
+  if options.dump:
     if options.json:
       sea.Export('Terms')
     else:
       seaice.pretty.printTermsPretty(sea, sea.getAllTerms(sortBy="term_string"))
 
   header = True
-  if options.score: 
+  if options.score:
     for term in sea.getAllTerms():
       (U, V) = sea.preScore(term['id'])
       s = sea.postScore(term['id'], U, V)
-      if header: 
+      if header:
         print "%-8s%-20s%-8s%-8s%-8s" % ('Id', 'Term', 'Up', 'Down', 'Score')
         header = False
-      print "%-8d%-20s%-8d%-8d%-8.2f" % (term['id'], 
-                                   term['term_string'], 
+      print "%-8d%-20s%-8d%-8d%-8.2f" % (term['id'],
+                                   term['term_string'],
                                    term['up'],
-                                   term['down'], 
+                                   term['down'],
                                    (100 * s))
-  
+
   header = True
   if options.classify:
     for (term, id) in map(lambda term: (term['term_string'], term['id']), sea.getAllTerms()):
-      if header: 
+      if header:
         print "%-8s%-20s%-8s" % ('Id', 'Term', 'Class')
         header = False
       print "%-8s%-20s%-8s" % (id, term, sea.classifyTerm(id))
       sea.commit()
-  
+
   if options.search_term:
     terms = sea.search(options.search_term)
-    if options.json: 
+    if options.json:
       seaice.pretty.printAsJSObject(terms)
-    elif len(terms) == 0: 
+    elif len(terms) == 0:
       print >>sys.stderr, "sea: no matches for '%s'" % options.search_term
     else: seaice.pretty.printTermsPretty(sea, terms)
 
@@ -228,7 +228,7 @@ try:
   sea.commit()
 
 except pqdb.DatabaseError, e:
-  print 'error: %s' % e    
+  print 'error: %s' % e
   sys.exit(1)
 
 # xxx commenting these out makes for better diagnostics?
@@ -236,5 +236,5 @@ except pqdb.DatabaseError, e:
 #  print >>sys.stderr, "error: file not found"
 #  sys.exit(1)
 #
-#except ValueError: 
+#except ValueError:
 #  print >>sys.stderr, "error: incorrect parameter type(s)"
